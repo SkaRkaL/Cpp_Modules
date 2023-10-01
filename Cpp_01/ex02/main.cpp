4155bef739c9840e096426b8ebb284407f3ff2c5
SkaRkaL <karkalsaad@gmail.com>
2023-10-01 08:29:54 +0100

    Brain Program

---
+++ Cpp_01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << &string << '\n';
+	std::cout << stringPTR << '\n';
+	std::cout << &stringREF << '\n';
+
+	std::cout << string << '\n';
+	std::cout << *stringPTR << '\n';
+	std::cout << stringREF << '\n';
+}
