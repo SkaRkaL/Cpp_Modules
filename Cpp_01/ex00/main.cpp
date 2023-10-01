74a9b0e2b0a4c4b1a326af21e6854ff405b2e53c
Saad Karkal <karkalsaad@gmail.com>
2023-10-01 07:34:14 +0100

    main Program

--- Cpp_01/ex00/main.cpp
+++ Cpp_01/ex00/main.cpp
@@ -1,44 +1,4 @@
-#include <iostream>
-#include <new>
-
-class	Zombie
-{
-	private:
-		std::string name;
-
-	public:
-		Zombie(std::string name);
-		~Zombie(void);
-		void	announce(void);
-};
-
-Zombie::Zombie(std::string name)
-{
-	this->name = name;
-}
-
-void Zombie::announce(void)
-{
-	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-Zombie::~Zombie(void)
-{
-	std::cout << this->name << " was destroyed" << std::endl;
-}
-
-Zombie	*newZombie(std::string name)
-{
-	Zombie *zo = new Zombie(name);
-	return (zo);
-}
-
-void	randomChump(std::string name)
-{
-	Zombie zo = Zombie(name);
-	zo.announce();
-}
-
+#include "include/Zombie.hpp"
 int main()
 {
 	Zombie *name;
