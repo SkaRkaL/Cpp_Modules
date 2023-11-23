fccb50c35f5b823d4b17cbc8d78c53db4d43e618
Saad Karkal <Karkalsaad@gmail.com>
2023-11-23 17:33:15 +0100

    Main file

--- Cpp_00/ex00/megaphone.cpp
+++ Cpp_00/ex00/megaphone.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
-int ft_strlen(char *str)
-{
-	int i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+int main(int ac, char **av) {
 
-int main(int ac, char **av)
-{
-	if (ac == 1)
-	{
+	std::string str;
+
+	if (ac == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		return (1);
 	}
-	char	*str;
-	for (int i = 1; i < ac; i++)
-	{
+
+	for (int i = 1; i < ac; i++) {
 		str = av[i];
-		for (int j = 0; j < ft_strlen(str); j++)
+		for (int j = 0; j < str.length(); j++)
 			std::cout << (char)toupper(str[j]);
+		std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
