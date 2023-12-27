a9574a609b2d44cc10964be44371a71a8cad950c
Saad Karkal <Karkalsaad@gmail.com>
2023-12-27 09:32:53 +0100

    WrongAnimal

--- Cpp_04/ex00/WrongAnimal.hpp
+++ Cpp_04/ex00/WrongAnimal.hpp
@@ -16,38 +16,3 @@ class WrongAnimal
 		~WrongAnimal();
 		WrongAnimal& operator=(const WrongAnimal& copy);
 };
-
-WrongAnimal::WrongAnimal()
-{
-	type = "WrongAnimal";
-	std::cout << "WrongAnimal default constructor called" << std::endl;
-}
-
-WrongAnimal::WrongAnimal(const WrongAnimal& copy)
-{
-	std::cout << "WrongAnimal copy constructor called" << std::endl;
-	*this = copy;
-}
-
-std::string WrongAnimal::getType() const
-{
-	return (type);
-}
-
-WrongAnimal	 &WrongAnimal::operator=(const WrongAnimal &copy)
-{
-	std::cout << "WrongAnimal Assignation Operator [=] Called" << std::endl;
-	if (this != &copy)
-		type = copy.type;
-	return (*this);
-}
-
-WrongAnimal::~WrongAnimal()
-{
-	std::cout << "WrongAnimal destructor called" << std::endl;
-}
-
-void WrongAnimal::makeSound() const
-{
-	std::cout << "WrongAnimal sound [ SOUND ]" << std::endl;
-}
