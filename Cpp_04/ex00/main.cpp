fd079591ce18a30f94c4bbee23ee51f932225abd
Saad Karkal <Karkalsaad@gmail.com>
2023-12-27 11:14:46 +0100

    main

--- Cpp_04/ex00/main.cpp
+++ Cpp_04/ex00/main.cpp
@@ -1,45 +1,49 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
 
 int main()
 {
-	const WrongAnimal* meta = new WrongAnimal();
-	std::cout << std::	endl;
-	std::cout << "===============" << std::endl;
-	const WrongAnimal* i = new WrongCat();
+	{
+		const Animal *Beta = new Animal();
+		std::cout << "-----------------" << std::endl;
+		const Animal *Dogyy = new Dog();
+		std::cout << "-----------------" << std::endl;
+		const Animal *Catyy = new Cat();
 
-	std::cout << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << "=====================================\\\\\\\\\\\\\\=============" << std::endl;
-	i->makeSound(); //will output the cat sound!
-	meta->makeSound();
+		std::cout << "---------- + + + + +  ----------" << std::endl;
+		std::cout << Dogyy->getType() << std::endl;
+		std::cout << Catyy->getType() << std::endl;
+		std::cout << Beta->getType() << std::endl;
+		std::cout << "---------- + + + + +  ----------" << std::endl;
+		Catyy->makeSound();
+		Dogyy->makeSound();
+		Beta->makeSound();
+		std::cout << std::endl;
+		delete Beta;
+		delete Dogyy;
+		delete Catyy;
+		std::cout << std::endl;
+	}
+		std::cout << "\x1b[31m==============   WrongAnimal Case   ============\033[0m\n" << std::endl;
+	{
+		const WrongAnimal* Beta = new WrongAnimal();
+		std::cout << "\x1b[36m--------------------------------------\x1b[0m" << std::endl;
+		const WrongAnimal* caty = new WrongCat();
 
-	std::cout << std::endl;
-	delete meta;
-	delete i;
+		std::cout << "\x1b[36m------------- + + + + +  -------------\x1b[0m" << std::endl;
+		std::cout << caty->getType() << std::endl;
+		std::cout << Beta->getType() << std::endl;
+		std::cout << "\x1b[36m------------- + + + + +  -------------\x1b[0m" << std::endl;
+
+		caty->makeSound();
+		Beta->makeSound();
+
+		std::cout << std::endl;
+		delete Beta;
+		delete caty;
+	}
 	return 0;
 }
-// int main()
-// {
-// 	const Animal* meta = new Animal();
-// 	std::cout << std::endl;
-// 	std::cout << "===============" << std::endl;
-// 	const Animal* j = new Dog();
-// 	std::cout << std::endl;
-// 	std::cout << "===============" << std::endl;
-// 	const Animal* i = new Cat();
-
-// 	std::cout << "=====================================\\\\\\\\\\\\\\=============" << std::endl;
-// 	std::cout << j->getType() << " " << std::endl;
-// 	std::cout << i->getType() << " " << std::endl;
-// 	std::cout << "=====================================\\\\\\\\\\\\\\=============" << std::endl;
-// 	i->makeSound(); //will output the cat sound!
-// 	j->makeSound();
-// 	meta->makeSound();
-
-// 	std::cout << std::endl;
-// 	delete meta;
-// 	delete j;
-// 	delete i;
-// 	return 0;
-// }
