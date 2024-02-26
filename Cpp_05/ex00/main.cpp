e1bee5cbb8d14cc840f3416b8f833fdf85f56aff
sakarkal <karkalsaad@gmail.com>
2024-02-26 14:42:03 +0100

    Main

---
+++ Cpp_05/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+
+	try {
+		Bureaucrat test("[ SkRkL ]", 1);
+		std::cout << test;
+		test.increment();
+		std::cout << test;
+	}
+	catch (const std::exception &excp) {
+		std::cerr << excp.what() << '\n';
+	}
+std::cout << "-------------------------------------\n" << std::endl;
+	try {
+		Bureaucrat test("[ SkRkL ]", 150);
+		std::cout << test;
+		test.decrement();
+		std::cout << test;
+	}
+	catch (const std::exception &excp) {
+		std::cerr << excp.what() << '\n';
+	}
+}
