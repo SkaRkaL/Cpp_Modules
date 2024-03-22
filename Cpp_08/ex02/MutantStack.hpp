2270cb89449769bc2b68c41df05135dfaedb4771
sakarkal <karkalsaad@gmail.com>
2024-03-22 23:08:49 +0000

    Done

--- Cpp_08/ex02/MutantStack.hpp
+++ Cpp_08/ex02/MutantStack.hpp
@@ -17,6 +17,7 @@ class MutantStack : public std::stack<T> {
 		~MutantStack() {}
 
 		typedef typename std::stack<T>::container_type::iterator iterator;
+
 		iterator begin()
 		{
 			return this->c.begin();
