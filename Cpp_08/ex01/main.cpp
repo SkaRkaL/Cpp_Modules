23dedc891bf56b57e68d02a27e2d2a71016df602
sakarkal <karkalsaad@gmail.com>
2024-03-23 01:23:58 +0000

    Done

--- Cpp_08/ex01/main.cpp
+++ Cpp_08/ex01/main.cpp
@@ -2,28 +2,28 @@
 
 int main()
 {
-try {
-    Span sp = Span(50);
-    std::vector<int> v;
+    try {
+        Span sp = Span(50);
+        std::vector<int> v;
 
-    for (int i = 1; i < 50 ; i++)
-    {
-        unsigned short j = random();
-        v.push_back(j);
+        for (int i = 1; i < 50 ; i++)
+        {
+            unsigned short j = random();
+            v.push_back(j);
+        }
+
+        sp.addNumbers(v.begin(), v.end());
+        // sp.addNumber(6);
+        // sp.addNumber(4);
+        // sp.addNumber(17);
+        // sp.addNumber(9);
+        // sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
-
-    sp.addNumbers(v.begin(), v.end());
-    // sp.addNumber(6);
-    // sp.addNumber(4);
-    // sp.addNumber(17);
-    // sp.addNumber(9);
-    // sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-}
-catch (std::exception &e)
-{
-    std::cout << e.what(); 
-}
+    catch (std::exception &e)
+    {
+        std::cout << e.what(); 
+    }
     return 0;
 }
