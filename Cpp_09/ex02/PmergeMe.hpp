2f937df7008e8bb96599fd76ad898c367c6c6263
Saad Karkal <karkalsaad@gmail.com>
2024-08-17 20:27:42 +0100

    some templates i will use

--- Cpp_09/ex02/PmergeMe.hpp
+++ Cpp_09/ex02/PmergeMe.hpp
@@ -2,21 +2,99 @@
 
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <list>
+#include <deque>
+#include <algorithm>
+#include <iterator>
 
-class PmergeMe
+using std::cout;
+using std::deque;
+using std::endl;
+using std::lower_bound;
+using std::make_pair;
+using std::pair;
+using std::vector;
+
+template <typename T>
+void binarySearchSort(T &left, T &right)
 {
-    public:
-        PmergeMe();
-        ~PmergeMe();
+	typename T::iterator it = left.begin();
 
-        void mergeVector(std::vector<int> &arr, int left, int middle, int right);
-        void mergeSortVector(std::vector<int> &arr, int left, int right);
+	for (size_t i = 0; i < right.size(); i++)
+	{
+		it = lower_bound(left.begin(), left.end(), right[i]);
+		left.insert(it, right[i]);
+	}
+}
 
-        void mergeList(std::list<int>::iterator left, std::list<int>::iterator middle, std::list<int>::iterator right);
-        void mergeSortList(std::list<int> &arr, std::list<int>::iterator left, std::list<int>::iterator right);
+template <typename T>
+void insertionSort(T &left)
+{
+	for (size_t i = 1; i < left.size(); i++)
+	{
+		int key = left[i];
+		int j = i - 1;
 
-        void printVector(const std::vector<int> &arr);
-        void printList(const std::list<int> &arr);
-};
+		while (j >= 0 && left[j] > key)
+		{
+			left[j + 1] = left[j];
+			j -= 1;
+		}
+		left[j + 1] = key;
+	}
+}
+
+template <typename PairType, typename T>
+void left_right(PairType pairs, T &left, T &right, int odd)
+{
+	for (size_t i = 0; i < pairs.size(); i += 2)
+	{
+		left.push_back(pairs[i].first);
+		right.push_back(pairs[i].second);
+	}
+	if (odd != -1)
+		left.push_back(odd);
+}
+
+template <typename PairType, typename T>
+PairType makePairs(T vec)
+{
+	PairType pairs;
+
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		if (i + 1 == vec.size())
+			break;
+		if (vec[i] > vec[i + 1])
+			pairs.push_back(make_pair(vec[i], vec[i + 1]));
+		else
+			pairs.push_back(make_pair(vec[i + 1], vec[i]));
+	}
+
+	return pairs;
+}
+
+template <typename T>
+int checkOddOrEven(T &vec)
+{
+	int odd = -1;
+
+	if (vec.size() % 2 == 1)
+		odd = vec[vec.size() - 1];
+	return odd;
+}
+
+template <typename C, typename P>
+void MergeInsertionSort(C &vec, C &left)
+{
+	P	pairs;
+	C	right;
+	int	odd;
+
+	odd = checkOddOrEven(vec);
+
+	pairs = makePairs<P>(vec);
+
+	left_right(pairs, left, right, odd);
+	insertionSort(left);
+	binarySearchSort(left, right);
+}
